d686c4c5e320a84b971671d79af65588d0da20dc
sunshiyun <897645431@qq.com>
2020-08-25 10:17:55 +0800

    demo

--- src/main/java/com/test/controller/HelloSpringboot.java
+++ src/main/java/com/test/controller/HelloSpringboot.java
@@ -1,7 +1,13 @@
 package com.test.controller;
+import com.test.util.ResultBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 /**
  * @Author ssy
  * @Description HelloSpringboot
@@ -10,10 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class HelloSpringboot {
+
+    private static final Logger logger  =  LoggerFactory.getLogger(HelloSpringboot.class );
+
     @RequestMapping("/hello")
     public String say() {
+        logger.info("我是sad={}","asdsa");
         System.out.println("Hello springboot");
+        logger.debug("Processing trade with id: {} and symbol : {} ", 11, 22);
+
+        logger.info("我是11sad={}","asdsa");
+        logger.info("我是1324sad={}","asdsa");
+        logger.info("我是342sad={}","asdsa");
+        try {
+            
+            int [] str = new int[2];
+            System.out.println(str[5]);
+        }catch (Exception e){
+            logger.error("2020-08-24 16:39:16.521 异常信息：{} {}",e.getStackTrace(),"2020-08-24 16:32:54.542");
+        }
+
         return "hello,this is a springboot demo！~";
+    }
+    @RequestMapping("/test")
+    public void  say11() {
+        throw new RuntimeException("ExceptionTest");
     }
 
     @RequestMapping("/login")
@@ -22,4 +49,6 @@ public class HelloSpringboot {
         System.out.println(result);
         return(result);
     }
+
+
 }
